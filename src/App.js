facbfb77451ca21074edcf417548b7f18b6e970d
Mateus Brito <mateusbrito4@gmail.com>
2024-10-16 16:22:38 -0300

    fix: routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Carteira from "./carteira.component";
 import fotoMateus from "./foto-mateus.jpeg";
 import fotoVal from "./foto-val.jpeg";
@@ -7,34 +7,32 @@ import fotoVal from "./foto-val.jpeg";
 function App() {
   return (
     <div className="App">
-      <Router basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route
-            path="/mateus"
-            element={
-              <Carteira
-                nome="MATEUS MEDEIROS DE ASSIS BRITO"
-                curso="Bacharelado em Engenharia da Computação"
-                rg="003036166"
-                dataNasc="04/12/1997"
-                foto={fotoMateus}
-              />
-            }
-          />
-          <Route
-            path="/val"
-            element={
-              <Carteira
-                nome="VALERIA GUIMARÃES FLORO"
-                curso="Bacharelado em Engenharia Biomédica"
-                rg="003461355"
-                dataNasc="17/11/1994"
-                foto={fotoVal}
-              />
-            }
-          />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route
+          path="/mateus"
+          element={
+            <Carteira
+              nome="MATEUS MEDEIROS DE ASSIS BRITO"
+              curso="Bacharelado em Engenharia da Computação"
+              rg="003036166"
+              dataNasc="04/12/1997"
+              foto={fotoMateus}
+            />
+          }
+        />
+        <Route
+          path="/val"
+          element={
+            <Carteira
+              nome="VALERIA GUIMARÃES FLORO"
+              curso="Bacharelado em Engenharia Biomédica"
+              rg="003461355"
+              dataNasc="17/11/1994"
+              foto={fotoVal}
+            />
+          }
+        />
+      </Routes>
     </div>
   );
 }
