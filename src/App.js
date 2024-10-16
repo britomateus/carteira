0de9123511f0d3cfc8fb84049251376feb3e032d
Mateus Brito <mateusbrito4@gmail.com>
2024-10-16 16:47:47 -0300

    fix: remove rotas

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
 import Carteira from "./carteira.component";
 import fotoMateus from "./foto-mateus.jpeg";
-import fotoVal from "./foto-val.jpeg";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Carteira
-              nome="MATEUS MEDEIROS DE ASSIS BRITO"
-              curso="Bacharelado em Engenharia da Computação"
-              rg="003036166"
-              dataNasc="04/12/1997"
-              foto={fotoMateus}
-            />
-          }
-        />
-        <Route
-          path="/val"
-          element={
-            <Carteira
-              nome="VALERIA GUIMARÃES FLORO"
-              curso="Bacharelado em Engenharia Biomédica"
-              rg="003461355"
-              dataNasc="17/11/1994"
-              foto={fotoVal}
-            />
-          }
-        />
-      </Routes>
-    </>
+    <div className="App">
+      <Carteira
+        nome="MATEUS MEDEIROS DE ASSIS BRITO"
+        curso="Bacharelado em Engenharia da Computação"
+        rg="003036166"
+        dataNasc="04/12/1997"
+        foto={fotoMateus}
+      />
+    </div>
   );
 }
 
