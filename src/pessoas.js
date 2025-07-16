5e56b84ee0ff520994abcfe481eae7d27bd457f4
britomateus <mateusbrito4@gmail.com>
2025-07-16 19:43:12 -0300

    fix: update user details in pessoas.js

--- src/pessoas.js
+++ src/pessoas.js
@@ -4,24 +4,24 @@ import fotoVal from "./foto-val.jpeg";
 
 export const pessoas = {
   janiely: {
-    nome: "JANIELY KELLY SILVA PEREIRA",
+    nome: "Janiely Kelly Silva Pereira",
     curso: "Bacharelado em Engenharia Biomédica",
     rg: "003.112.198",
     dataNasc: "12/05/1997",
     foto: fotoJanis
   },
   mateus: {
-    nome: "MATEUS BRITO SILVA",
-    curso: "Bacharelado em Ciência da Computação",
-    rg: "003.112.199",
-    dataNasc: "15/03/1995",
+    nome: "Mateus Medeiros de Assis Brito",
+    curso: "Bacharelado em Engenharia da Computação",
+    rg: "003.036.166",
+    dataNasc: "04/12/1997",
     foto: fotoMateus
   },
   valeria: {
-    nome: "VALÉRIA COSTA SANTOS",
-    curso: "Bacharelado em Administração",
-    rg: "003.112.200",
-    dataNasc: "20/08/1998",
+    nome: "Valéria Guimarães Floro",
+    curso: "Bacharelado em Engenharia Biomédica",
+    rg: "003.036.166",
+    dataNasc: "04/12/1997",
     foto: fotoVal
   }
 }; 
