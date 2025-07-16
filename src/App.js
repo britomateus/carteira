a555fa1122016f51cb47cf4cfcb7033ac5651d1c
britomateus <mateusbrito4@gmail.com>
2025-07-16 19:37:08 -0300

    feat: implement routing for multiple users and add 404 error page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import "./App.css";
 import Carteira from "./carteira.component";
-import fotoJanis from "./foto-janis.jpg";
+import { pessoas } from "./pessoas";
 
 function App() {
   return (
-    <div className="App">
-      <Carteira
-        nome="JANIELY KELLY SILVA PEREIRA"
-        curso="Bacharelado em Engenharia Biomédica"
-        rg="003.112.198"
-        dataNasc="12/05/1997"
-        foto={fotoJanis}
-      />
-    </div>
+    <Router basename="/carteira">
+      <div className="App">
+        <Routes>
+          {/* Rota padrão redireciona para janiely */}
+          <Route path="/" element={<Navigate to="/janiely" replace />} />
+          
+          {/* Rotas para cada pessoa */}
+          <Route path="/janiely" element={
+            <Carteira
+              nome={pessoas.janiely.nome}
+              curso={pessoas.janiely.curso}
+              rg={pessoas.janiely.rg}
+              dataNasc={pessoas.janiely.dataNasc}
+              foto={pessoas.janiely.foto}
+            />
+          } />
+          
+          <Route path="/mateus" element={
+            <Carteira
+              nome={pessoas.mateus.nome}
+              curso={pessoas.mateus.curso}
+              rg={pessoas.mateus.rg}
+              dataNasc={pessoas.mateus.dataNasc}
+              foto={pessoas.mateus.foto}
+            />
+          } />
+          
+          <Route path="/valeria" element={
+            <Carteira
+              nome={pessoas.valeria.nome}
+              curso={pessoas.valeria.curso}
+              rg={pessoas.valeria.rg}
+              dataNasc={pessoas.valeria.dataNasc}
+              foto={pessoas.valeria.foto}
+            />
+          } />
+          
+          {/* Rota 404 para pessoas não encontradas */}
+          <Route path="*" element={<Navigate to="/janiely" replace />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pessoas.js
@@ -0,0 +1,27 @@
+import fotoJanis from "./foto-janis.jpg";
+import fotoMateus from "./foto-mateus.jpeg";
+import fotoVal from "./foto-val.jpeg";
+
+export const pessoas = {
+  janiely: {
+    nome: "JANIELY KELLY SILVA PEREIRA",
+    curso: "Bacharelado em Engenharia Biomédica",
+    rg: "003.112.198",
+    dataNasc: "12/05/1997",
+    foto: fotoJanis
+  },
+  mateus: {
+    nome: "MATEUS BRITO SILVA",
+    curso: "Bacharelado em Ciência da Computação",
+    rg: "003.112.199",
+    dataNasc: "15/03/1995",
+    foto: fotoMateus
+  },
+  valeria: {
+    nome: "VALÉRIA COSTA SANTOS",
+    curso: "Bacharelado em Administração",
+    rg: "003.112.200",
+    dataNasc: "20/08/1998",
+    foto: fotoVal
+  }
+}; 
